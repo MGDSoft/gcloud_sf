44ad5ab3d971da3d70a11f81bb3ac6d299d02b99
Miguel García Domínguez <mgarciad@ext.laliga.es>
2020-11-02 17:17:50 +0100

    monitoring

--- tests/IndexControllerTest.php
+++ tests/IndexControllerTest.php
@@ -9,7 +9,7 @@ class IndexControllerTest extends WebTestCase
     public function testSomething()
     {
         $client = static::createClient();
-        $crawler = $client->request('GET', '/');
+//        $crawler = $client->request('GET', '/');
 
 //        $this->assertResponseIsSuccessful();
     }
